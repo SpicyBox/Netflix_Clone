26914a6f67b740b2c0670d40c28b69c9e9340926
Leejeongchan1733026 <cksqls288@gmail.com>
2022-06-28 11:32:14 +0900

    트레일러 추가 및 다운로드 버튼 추가

--- Netflix Clone/Views/CollectioinViewTableViewCell.swift
+++ Netflix Clone/Views/CollectioinViewTableViewCell.swift
@@ -52,6 +52,10 @@ class CollectioinViewTableViewCell: UITableViewCell {
             self?.collectionView.reloadData()
         }
     }
+    
+    private func downloadTitleAt(indexPath: IndexPath) {
+        print("Downloading \(titles[indexPath.row].original_title)")
+    }
 }
 
 extension CollectioinViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -96,4 +100,17 @@ extension CollectioinViewTableViewCell: UICollectionViewDelegate, UICollectionVi
             }
         }
     }
-}
+    
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point:
+        CGPoint) -> UIContextMenuConfiguration? {
+            let config = UIContextMenuConfiguration(
+                identifier: nil,
+                previewProvider: nil) {[weak self] _ in
+                    let downloadAction = UIAction(title: "Download", subtitle: nil, image: nil, identifier: nil, discoverabilityTitle: nil, state: .off) { _ in
+                        self?.downloadTitleAt(indexPath: indexPath)
+                    }
+                    return UIMenu(title: "", image: nil, identifier: nil, options: .displayInline, children: [downloadAction])
+                }
+            return config
+        }
+    }
